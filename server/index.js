2c3ea1f1b48522f679bf00074ce65f6884640519
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-01-07 19:30:48 +0800

    Setup All

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -8,6 +7,4 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
